3a899190544fa34bbec54d62bb981232d54070d2
aman-saurabh <asaurabh001@gmail.com>
2021-11-09 01:51:40 +0530

    Completed header, footer and layout structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: '', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
   { path: 'post', loadChildren: () => import('./modules/post/post.module').then(m => m.PostModule) }
 ];
 
