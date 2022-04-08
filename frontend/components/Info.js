ed7e3e23541940b5411f3236d829c3cd2cf43517
techpassel <techpasselorg@gmail.com>
2022-04-08 17:24:28 +0530

    Made some changes in design

--- frontend/components/Info.js
+++ frontend/components/Info.js
@@ -11,10 +11,10 @@ const Info = () => {
             <p className={styles.details}>Technower is a collaboration platform for technophiles.
                 It is a platform where technowers collaborate together to make the learning and understanding
                 of latest technologies easy and accessible for everyone. They use this platform to share
-                their knowledge and skills with the world and to excel their skills by grabing the knowledge
-                shared by fellow technowers. They also use this platform for many other purposes like for getting
+                their knowledge and skills with the world and to excel in their skills by grabing the knowledge
+                shared by others. They also use this platform for many other purposes like for getting
                 assistance from the fellow technowers in the time of need or sometimes just to find technowers with
-                matching vigor, passion and interest.
+                matching interest, vigor and passion.
             </p>
             <button className={styles.button}>Learn more
                 <FontAwesomeIcon style={{ 'marginLeft': "5px" }} icon={faArrowRight} />
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,9 +5,13 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <div className="main-body"><Component {...pageProps} /></div>
-  </Layout>
+  return (
+    <Layout>
+      <div className="main-body">
+        <Component {...pageProps} />
+      </div>
+    </Layout>
+  )
 }
 
 export default MyApp
